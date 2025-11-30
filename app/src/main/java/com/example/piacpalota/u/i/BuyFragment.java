648f1bf8e37ad7d1e65be137db5bb3ddfd063683
OldJoseph8 <erik.kovacs81@gmial.com>
2025-11-30 15:41:40 +0100

    initial commit

--- app/src/main/java/com/example/piacpalota/u/i/BuyFragment.java
+++ app/src/main/java/com/example/piacpalota/u/i/BuyFragment.java
@@ -11,8 +11,6 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-// FONTOS: Be kell importálni a MainActivity-t, hogy elérjük a navigateTo függvényt
-import com.example.piacpalota.MainActivity;
 import com.example.piacpalota.R;
 import com.example.piacpalota.u.i.buylist.BuyAdapter;
 import com.example.piacpalota.u.i.buylist.CarRepository;
@@ -34,61 +32,57 @@ public class BuyFragment extends Fragment {
         recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        // Adatok betöltése a közös tárolóból
-        // Így látni fogod a SalesFragment-en hozzáadott új autókat is
+        // Adatok betöltése
         buyList = CarRepository.getInstance().getProducts();
 
         buyAdapter = new BuyAdapter(getContext(), buyList, new BuyAdapter.OnProductClickListener() {
 
-            // 1. KOSÁRBA GOMB
             @Override
             public void onAddToCartClick(Product product) {
-                navigateWithProduct(product, R.id.shoppingFragment);
+                // Itt most a ShoppingFragment-et nyitjuk meg kézzel
+                openFragment(new ShoppingFragment(), product);
             }
 
-            // 2. RÉSZLETEK GOMB - EZ VISZ ÁT AZ ÚJ OLDALRA!
             @Override
             public void onDetailsClick(Product product) {
-                navigateWithProduct(product, R.id.carDetailFragment);
+                // Itt pedig a CarDetailFragment-et (Részletek)
+                openFragment(new CarDetailFragment(), product);
             }
         });
 
         recyclerView.setAdapter(buyAdapter);
-
         return view;
     }
 
-    // Segédfüggvény a navigációhoz és adatátadáshoz
-    // Ez csomagolja össze az autó adatait és kéri meg a MainActivity-t a lapozásra
-    private void navigateWithProduct(Product product, int destinationId) {
+    // --- EZ AZ ÚJ, "BOMBABIZTOS" KÉZI NAVIGÁCIÓ ---
+    private void openFragment(Fragment fragment, Product product) {
         try {
+            // 1. Adatok becsomagolása
             Bundle bundle = new Bundle();
             bundle.putString("productName", product.getName());
             bundle.putString("productPrice", product.getPrice());
             bundle.putString("productQuantity", product.getQuantity());
             bundle.putString("productLocation", product.getLocation());
             bundle.putString("productDescription", product.getDescription());
-
-            // Képek listájának átadása
-            // Fontos: ArrayList-té kell alakítani a List-et, mert a Bundle azt várja
             bundle.putStringArrayList("productImages", new ArrayList<>(product.getImages()));
 
-            // A MainActivity-t kérjük meg a biztonságos lapozásra
-            if (getActivity() instanceof MainActivity) {
-                ((MainActivity) getActivity()).navigateTo(destinationId, bundle);
-            } else {
-                Log.e("BuyFragment", "Hiba: Az Activity nem MainActivity!");
-            }
+            fragment.setArguments(bundle);
+
+            // 2. A képernyő lecserélése "erőszakkal" (kikerülve a NavController-t)
+            requireActivity().getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.nav_host_fragment, fragment) // A fő keretbe tesszük
+                    .addToBackStack(null) // Hogy a vissza gomb működjön
+                    .commit();
 
         } catch (Exception e) {
-            Log.e("BuyFragment", "Navigációs hiba: " + e.getMessage());
+            Log.e("BuyFragment", "Hiba a megnyitáskor: " + e.getMessage());
         }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        // Frissítjük a nézetet visszalépéskor, hogy az új adatok látszódjanak
         if (buyAdapter != null) {
             buyAdapter.notifyDataSetChanged();
         }
